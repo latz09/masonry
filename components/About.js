4ae8430949f9ef37ad42d1b95d5409505414b3fb
latz09 <85132328+latz09@users.noreply.github.com>
2023-06-09 17:01:04 -0500

    hero

--- components/About.js
+++ components/About.js
@@ -8,7 +8,7 @@ const About = () => {
 
 	return (
 		<div className='max-w-4xl tracking-wider lg:leading-9  mx-auto lg:text-xl text-justify text-dark  grid '>
-			<div className="px-4 text-center">
+			<div className="px-2 text-center">
 				We are all about bringing your{' '}
 				<span className='font-bold text-primary'>
 					{' '}
--- components/WelcomeHero.js
+++ components/WelcomeHero.js
@@ -4,12 +4,10 @@ import About from './About';
 const WelcomeHero = () => {
 	return (
 		<div className="bg-gradient-to-b from-light via-[#FEF9F6] to-light">
-			<div className=' mx-4'>
-				<TopBanner />
-			</div>
-			<div className='  h-[60vh]  border-primary g lg:gap-0 flex flex-col justify-center gap-12 lg:justify-around'>
+		
+			<div className='  h-[65vh]  border-primary g lg:gap-0 flex flex-col justify-center gap-12 lg:justify-around'>
 				<BusinessName />
-				<CTA />
+				<TopBanner />
 				<About />
 			</div>
 		</div>
@@ -24,7 +22,7 @@ export default WelcomeHero;
 
 const TopBanner = () => {
 	return (
-		<div className=' text-lg text-end max-w-7xl mx-auto py-3 lg:text-3xl tracking-wider font-thin scale-y- text-secondary'>
+		<div className=' text-lg text-end max-w-7xl mx-auto  lg:text-3xl tracking-wider font-thin scale-y- text-secondary'>
 			<span>651 252-1819</span>
 		</div>
 	);
@@ -39,7 +37,7 @@ const BusinessName = () => {
 				</div>
 				<div className="text-sm md:text-base">Masonry in St. Paul, MN</div>
 			</div>
-			<div className='text-xl lg:text-3xl opacity-80 scale-y-150'>
+			<div className='text-xl lg:text-3xl opacity-80 scale-y-150 text-center'>
 				{' '}
 				From Deterioration Investigation to Full Restoration.
 			</div>
