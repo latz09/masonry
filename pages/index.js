92a87754afbf6dc7ddfadf941b2fb2d0ac79a5a5
latz09 <85132328+latz09@users.noreply.github.com>
2024-03-08 15:58:12 -0600

    d

--- pages/index.js
+++ pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
 				animate={{ opacity: 1, y: 0 }}
 				transition={{ duration: 1.5, delay: 0.3 }}
 			>
-				<LandingBanner />
+				<LandingBanner  />
 			</motion.div>
 			<WelcomeHero />{' '}
 			<div
